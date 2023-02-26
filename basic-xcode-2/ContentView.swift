6e31dd7317dfa60ee8c1922470ac720c6e27b0ad
Rusydy <rusydymuhiddin@gmail.com>
2023-02-26 22:32:15 +0700

    build the card background

--- basic-xcode-2/ContentView.swift
+++ basic-xcode-2/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack(alignment: .leading, spacing: 20.0){
             Image("niagarafalls")
                 .resizable()
@@ -20,16 +21,43 @@ struct ContentView: View {
                     .font(.title)
                     .fontWeight(.bold)
                 
-                Image(systemName: "star.fill")
-                Image(systemName: "star.fill")
-                Image(systemName: "star.fill")
-                Image(systemName: "star.fill")
-                Image(systemName: "star.leadinghalf.filled")
+                Spacer()
+                
+                VStack {
+                    
+                    HStack {
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.leadinghalf.filled")
+                    }
+                    
+                    Text("Reviews (340)")
+                }
+                .foregroundColor(.orange)
+                .font(.caption)
                 
             }
             
             Text("Come visit the falls for an exeprience of a lifetime!")
+            
+            HStack{
+                Spacer()
+                Image(systemName: "fork.knife")
+                Image(systemName: "binoculars.fill")
+            }
+            .foregroundColor(.gray)
+            .font(.caption)
         }
+        .padding()
+        .background(Rectangle()
+            .foregroundColor(.white)
+            .cornerRadius(15)
+            .shadow(radius: 15)
+        )
+        .padding()
+        
     }
 }
 
