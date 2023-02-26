077efe1cb371c8d1ab0f3518e28e6add98d8aefd
Rusydy <rusydymuhiddin@gmail.com>
2023-02-26 22:34:31 +0700

    build the view background

--- basic-xcode-2/ContentView.swift
+++ basic-xcode-2/ContentView.swift
@@ -10,53 +10,58 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack(alignment: .leading, spacing: 20.0){
-            Image("niagarafalls")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(15)
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea()
             
-            HStack {
-                Text("Niagara Falls")
-                    .font(.title)
-                    .fontWeight(.bold)
+            VStack(alignment: .leading, spacing: 20.0){
+                Image("niagarafalls")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
                 
-                Spacer()
-                
-                VStack {
+                HStack {
+                    Text("Niagara Falls")
+                        .font(.title)
+                        .fontWeight(.bold)
                     
-                    HStack {
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.leadinghalf.filled")
+                    Spacer()
+                    
+                    VStack {
+                        
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        
+                        Text("Reviews (340)")
                     }
+                    .foregroundColor(.orange)
+                    .font(.caption)
                     
-                    Text("Reviews (340)")
                 }
-                .foregroundColor(.orange)
-                .font(.caption)
                 
+                Text("Come visit the falls for an exeprience of a lifetime!")
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
             }
-            
-            Text("Come visit the falls for an exeprience of a lifetime!")
-            
-            HStack{
-                Spacer()
-                Image(systemName: "fork.knife")
-                Image(systemName: "binoculars.fill")
-            }
-            .foregroundColor(.gray)
-            .font(.caption)
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(15)
+                .shadow(radius: 15)
+            )
+            .padding()
         }
-        .padding()
-        .background(Rectangle()
-            .foregroundColor(.white)
-            .cornerRadius(15)
-            .shadow(radius: 15)
-        )
-        .padding()
         
     }
 }
